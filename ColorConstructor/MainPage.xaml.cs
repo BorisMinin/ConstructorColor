8aa9fa3504f50a6959f1d2134409fa29d2f052bf
Борис Минин <boris.minin@outlook.com>
2018-03-23 17:20:55 +0300

    Добавлен коментарий

--- ColorConstructor/MainPage.xaml.cs
+++ ColorConstructor/MainPage.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Navigation;
 using Windows.UI;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
+// Добавлен проект на GitHub
 namespace ColorConstructor
 {
     /// <summary>
